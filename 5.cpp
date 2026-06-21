75cfce881956f81e5ac6de7ef11e2f36c518a0d3
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-06-22 01:18:16 +0530

    New pattern problem, Which is for printing new numnber everytime

---
+++ 5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+
+    //    1 2 3
+    //    4 5 6
+    //    7 8 9
+
+    // .Pattern problem 
+
+    int n;
+    cout << "enter the value of n :";
+    cin >> n;
+    int num =0 ;
+
+    for (int i =0 ; i<=n -1;i++){   // Outer loop for lines 
+
+
+            for (int j=1; j<=n ; j++){  // IOne row print n number of value 
+
+                num++;  // Num variable adds value by 1 
+                cout << num;
+
+            }
+            cout << endl;   
+    }
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
