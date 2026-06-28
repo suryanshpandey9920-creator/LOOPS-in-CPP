5cbfaee6a1c6c1ac6f7a0d707b679eb7f762849d
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-06-28 15:14:21 +0530

    Pyramid pattern problem & Reverse triangle problem solved

---
+++ 11.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    //  PPyramid Pattern problem 
+
+    //       1
+    //     1 2 1
+    //   1 2 3 2 1
+    // 1 2 3 4 3 2 1
+
+    int n;
+    cout <<"  enter the value of n : ";
+    cin >> n;
+    
+    for (int i = 0 ; i< n ; i++) {
+        // Inverted triangle space
+        for (int j= 0; j< n-i ; j++){
+            cout << " ";
+        }
+        // Number in 1 line printing 
+        for (int j=1 ; j< i ;j++){
+            cout << j ; 
+        }
+        // Right angle triangle 
+        for (int k =i; k>0 ;k--){
+            cout << k ;
+        }
+        // end of line 
+        cout << endl;
+
+    }
+
+    return 0;
+}
---
+++ 12.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std ;
+
+int main() {
+    // 1
+    // 1 2
+    // 1 2 3 
+    // 1 2 3 4 
+ // Reverse triangle pattern
+     int n;
+     cout << "enter the value of n : ";
+     cin >> n; 
+
+     for (int i =0; i<=n ; i++){
+        for (int j = i ; j>0 ; j--){
+            cout << j;
+            
+        }
+        cout << endl;
+     }
+
+    return 0;
+}
